df20393a1f2278fab88cecfc88d4d6c27fe1fec2
CHOI-MINGI <alrltjd1810@gmail.com>
2024-10-07 21:35:11 +0900

    Add files via upload

---
+++ 10951.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    while(s.hasNextInt()) {
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int c = A+B;
+        System.out.println(C);
+    }
+        }
+    }
+
+    
